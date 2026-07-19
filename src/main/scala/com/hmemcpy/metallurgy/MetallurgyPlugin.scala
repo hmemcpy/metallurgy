0e570551847316d42e4a38d32bd4fbd699c5c1d6
Igal Tabachnik <hmemcpy@gmail.com>
2026-07-19 11:42:47 +0300

    Defer module scan to background thread to avoid blocking EDT on startup

--- src/main/scala/com/hmemcpy/metallurgy/MetallurgyPlugin.scala
+++ src/main/scala/com/hmemcpy/metallurgy/MetallurgyPlugin.scala
@@ -5,17 +5,31 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.util.Disposer
 import org.jetbrains.plugins.scala.project._
 
 final class MetallurgyStartupActivity extends StartupActivity {
   override def runActivity(project: Project): Unit = {
     MetallurgyPlugin.Log.info("Metallurgy loaded")
-    val detection = ModuleDetectionService.get(project)
-    // Scan existing modules for Scala 3.5+ eligibility and fire first-detection notification
-    // for each one the user hasn't been asked about.
-    ModuleManager.getInstance(project).getModules
-      .filter(detection.isEligible)
-      .foreach(FirstDetectionNotifier.notify)
+    val indicator = new java.lang.Object
+    val thread = new Thread(() => {
+      try { Thread.sleep(10000) } catch { case _: InterruptedException => return }
+      if (project.isDisposed) return
+      scanModules(project)
+    }, "Metallurgy-Startup-Scan")
+    thread.setDaemon(true)
+    thread.start()
+  }
+
+  private def scanModules(project: Project): Unit = {
+    try {
+      val detection = ModuleDetectionService.get(project)
+      ModuleManager.getInstance(project).getModules
+        .filter(detection.isEligible)
+        .foreach(FirstDetectionNotifier.notify)
+    } catch {
+      case e: Exception => MetallurgyPlugin.Log.warn(s"Module scan failed: $e")
+    }
   }
 }
 
